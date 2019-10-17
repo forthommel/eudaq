7dcd5d5c75cc03e2979e75c43b507417dbcb1cbc
Laurent Forthomme <laurent.forthomme@cern.ch>
2019-10-17 11:25:35 +0200

    Fixed indentation

--- main/lib/ttree/src/TTreeEventConverter.cc
+++ main/lib/ttree/src/TTreeEventConverter.cc
@@ -19,24 +19,24 @@ namespace eudaq{
       d2->Branch("run_n", &run_n, "run_n/I");
       d2->Branch("event_n", &event_n, "event_n/I");
       if(d1->IsFlagTimestamp()){
-  uint64_t tsb = (int)d1->GetTimestampBegin();
-  d2->SetBranchAddress("timestampbegin", &tsb);
-  uint64_t tse = (int)d1->GetTimestampEnd();
-  d2->SetBranchAddress("timestampend", &tse);
+        uint64_t tsb = (int)d1->GetTimestampBegin();
+        d2->SetBranchAddress("timestampbegin", &tsb);
+        uint64_t tse = (int)d1->GetTimestampEnd();
+        d2->SetBranchAddress("timestampend", &tse);
       }
       uint32_t eflag = d1->GetFlag();
       d2->SetBranchAddress("event_flag", &eflag);
       uint32_t devnum = d1->GetDeviceN();
       d2->SetBranchAddress("device_n", & devnum);
       if(d1->IsFlagTrigger()){
-  uint32_t trign = d1->GetTriggerN();
-  d2->SetBranchAddress("trigger_n", &trign);
+        uint32_t trign = d1->GetTriggerN();
+        d2->SetBranchAddress("trigger_n", &trign);
       }
       size_t nsub = d1->GetNumSubEvent();
       for(size_t i=0; i<nsub; i++){
-  auto subev = d1->GetSubEvent(i);
-  if(!TTreeEventConverter::Convert(subev, d2, conf))
-    return false;
+        auto subev = d1->GetSubEvent(i);
+        if(!TTreeEventConverter::Convert(subev, d2, conf))
+          return false;
       }
       d2_flag =  (int)(d1->GetFlag() & ~Event::Flags::FLAG_PACK);
       return true;
@@ -46,23 +46,23 @@ namespace eudaq{
       uint32_t run_n = (d1->GetRunN());
       d2->SetBranchAddress("run_n",&run_n);
       uint32_t event_n = (d1->GetEventN());
-           d2->SetBranchAddress("event_n", &event_n);
+      d2->SetBranchAddress("event_n", &event_n);
       if(d1->IsFlagTimestamp()){
-  uint64_t tsb = (int)d1->GetTimestampBegin();
-  d2->SetBranchAddress("timestampbegin", &tsb);
-  uint64_t tse = (int)d1->GetTimestampEnd();
-  d2->SetBranchAddress("timestampend", &tse);
+        uint64_t tsb = (int)d1->GetTimestampBegin();
+        d2->SetBranchAddress("timestampbegin", &tsb);
+        uint64_t tse = (int)d1->GetTimestampEnd();
+        d2->SetBranchAddress("timestampend", &tse);
       }
-  uint32_t eflag = d1->GetFlag();
-  d2->SetBranchAddress("event_flag", &eflag);
-  uint32_t devnum = d1->GetDeviceN();
-  d2->SetBranchAddress("device_n", & devnum);
-  uint32_t trign = d1->GetTriggerN();
-  d2->SetBranchAddress("trigger_n", &trign);
-  if(d1->IsFlagTrigger()){
-    uint32_t trign = d1->GetTriggerN();
-    d2->SetBranchAddress("trigger_n", &trign);
-  }
+      uint32_t eflag = d1->GetFlag();
+      d2->SetBranchAddress("event_flag", &eflag);
+      uint32_t devnum = d1->GetDeviceN();
+      d2->SetBranchAddress("device_n", & devnum);
+      uint32_t trign = d1->GetTriggerN();
+      d2->SetBranchAddress("trigger_n", &trign);
+      if(d1->IsFlagTrigger()){
+        uint32_t trign = d1->GetTriggerN();
+        d2->SetBranchAddress("trigger_n", &trign);
+      }
     }
 
     uint32_t id = d1->GetType();
