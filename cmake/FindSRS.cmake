f8760f6d78f62f98269ca18856e707f24244e836
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-01-24 17:31:49 +0100

    FindSRS script added

---
+++ cmake/FindSRS.cmake
@@ -0,0 +1,29 @@
+# - Try to find the SRS driver
+# Once done this will define
+#  SRS_FOUND - System has its SRS library
+#  SRS_INCLUDE_DIR - The SRS library main include directories
+#  SRS_LIBRARY - The libraries needed to use SRS readout library
+
+message(STATUS "Looking for SRS driver.")
+
+set(SRS_DIRS $ENV{SRS_DIR} ${SRS_DIR})
+find_path(SRS_INCLUDE_DIR NAMES "srsdriver/Config.h" PATHS "${SRS_DIRS}")
+message(STATUS "srsdriver => ${SRS_INCLUDE_DIR}")
+if(SRS_INCLUDE_DIR)
+   set(SRS_INC_FOUND TRUE)
+   message(STATUS "Found SRS driver headers: ${SRS_INCLUDE_DIR}")
+endif()
+
+find_library(SRS_LIBRARY NAMES "libsrsdriver.so" HINTS "${SRS_DIRS} ${SRS_DIRS}/build")
+message(STATUS "libsrsdriver.so => ${SRS_LIBRARY}")
+if(SRS_LIBRARY)
+   set(SRS_LIB_FOUND TRUE)
+   message(STATUS "Found SRS driver library: ${SRS_LIBRARY}")
+endif()
+
+if(SRS_LIB_FOUND AND SRS_INC_FOUND)
+   set(SRS_FOUND TRUE)
+   message(STATUS "SRS driver found")
+endif()
+
+mark_as_advanced(SRS_LIBRARY SRS_INCLUDE_DIR)
