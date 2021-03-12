36d4c546763b5e1e232861457fad383dafd9368e
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-03-12 16:15:25 +0100

    Added max amplitude vistar

--- user/srs/module/src/SrsMonitor.cc
+++ user/srs/module/src/SrsMonitor.cc
@@ -32,6 +32,7 @@ private:
   srs::SystemRegister m_sys;
   srs::ApvAppRegister m_apvapp;
   std::map<unsigned short, TGraph*> m_map_ch_framebuf;
+  std::map<unsigned short, TH1D*> m_map_ch_maxampl;
 };
 
 namespace{
@@ -44,9 +45,10 @@ SrsMonitor::SrsMonitor(const std::string & name, const std::string & runcontrol)
 }
 
 void SrsMonitor::AtConfiguration(){
-  { // retrieve run configuration directly for SRS
+  auto cfg = GetConfiguration();
+  if (cfg) {
+    // retrieve run configuration directly for SRS
     // (as a fallback if not retrieved from data stream)
-    auto cfg = GetConfiguration();
     const std::string addr_server = cfg->Get("SRS_SERVER_ADDR", "10.0.0.2");
     if (addr_server.empty())
       EUDAQ_THROW("Failed to retrieve the SRS server address!");
@@ -82,16 +84,28 @@ void SrsMonitor::AtEventReception(eudaq::EventSP ev){
     return;
 
   event->Print(std::cout);
-  /*srs::SrsData* data = nullptr;
-  while (data = event->Data()->nextPtr())
-    data->print(std::cout);*/
-  const auto ch_id = event->Data()->daqChannel();
+  const auto ch_id = event->Data()->daqChannel(); //FIXME
   if (m_map_ch_framebuf.count(ch_id) == 0) {
-    m_map_ch_framebuf[ch_id] = m_monitor->Book<TGraph>("framebuf_vs_ch", "Frame buffer");
+    m_map_ch_framebuf[ch_id] = m_monitor->Book<TGraph>(Form("channel %zu/framebuf", ch_id), "Frame buffer");
     m_map_ch_framebuf[ch_id]->SetTitle(";Time slice;ADC count");
+    //m_monitor->SetPersistant(m_map_ch_framebuf[ch_id], false);
+    m_map_ch_maxampl[ch_id] = m_monitor->Book<TH1D>(Form("channel %zu/maxampl", ch_id), "Max.amplitude", Form("maxampl_ch%zu", ch_id), Form("Channel %zu;Max. amplitude;Events", ch_id), 100, 0., 2000.);
   }
   if (event->Data()->dataSource() == srs::SrsFrame::ADC) {
     const auto adc_frm = event->Data()->next<srs::AdcData>();
-    adc_frm.print(std::cout);
+    int i = 0;
+    float max_ampl = -1.;
+    while (true) {
+      try {
+        const auto frms = adc_frm.next();
+        for (const auto& frm : frms.data) {
+          m_map_ch_framebuf[ch_id]->SetPoint(m_map_ch_framebuf[ch_id]->GetN(), i++, (float)frm);
+          max_ampl = std::max(max_ampl, (float)frm);
+        }
+        m_map_ch_maxampl[ch_id]->Fill(max_ampl);
+      } catch (const srs::SrsFrame::NoMoreFramesException&) {
+        break;
+      }
+    }
   }
 }
